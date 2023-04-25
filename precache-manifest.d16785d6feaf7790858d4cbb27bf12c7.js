38c0705ebef266cb72eea2f27e53a0640622318c
matel <matelajos99@gmail.com>
2023-04-25 22:44:39 +0100

    Updates

---
+++ precache-manifest.d16785d6feaf7790858d4cbb27bf12c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f14a6069505c96ead716",
+    "url": "/survivalgameblog/static/css/main.4b788869.chunk.css"
+  },
+  {
+    "revision": "f14a6069505c96ead716",
+    "url": "/survivalgameblog/static/js/main.f14a6069.chunk.js"
+  },
+  {
+    "revision": "28b8c4b695364e983253",
+    "url": "/survivalgameblog/static/js/1.28b8c4b6.chunk.js"
+  },
+  {
+    "revision": "9d38cae401450f50381d",
+    "url": "/survivalgameblog/static/js/runtime~main.9d38cae4.js"
+  },
+  {
+    "revision": "0a7ee7942eb3341d79910b4ceb158ef6",
+    "url": "/survivalgameblog/index.html"
+  }
+];
